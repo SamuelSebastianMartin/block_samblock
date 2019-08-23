93f14d97f5bd38e88cd4d84c4eb1992b835dd9f6
Samuel Martin <Samuel Martin>
2019-08-23 01:33:59 +0100

    Working version of skeleton block

---
+++ lang/en/block_samblock.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'samblock block';
+$string['samblock'] = 'samblock';
+$string['samblock:addinstance'] = 'Add a new samblock block';
+$string['samblock:myaddinstance'] = 'Add a new samblock block to the My Moodle page';
